34cef57e3314854d3e02a0193976c6bddd643604
BlueSugar <emerald620blue@gmail.com>
2025-06-29 12:07:45 +0900

    update

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,4 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.g.rustfmt_autosave = true
+vim.g.terraform_fmt_on_save = true
--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,9 +1,6 @@
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cp app && yarn install",
-    init = function()
-        vim.g.mkdp_filetypes = { "markdown" }
-    end,
+    build = function() vim.fn["mkdp#util#install"]() end,
     ft = { "markdown" },
 }
