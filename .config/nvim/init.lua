d899bea289e5682f2d7b105170dcd85977076873
chiaoi <chiaoi@pm.me>
2025-09-30 06:53:36 +0900

    chore: use SSH key in actions clone

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,7 +9,8 @@ require("keymaps")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "git@github.com:folke/lazy.nvim"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local out =
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
